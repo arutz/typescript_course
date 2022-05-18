331712fa1e05f3d06c1e06a3a52ac1f4fd8af49c
Achim Rutz <achim.rutz@mt.com>
2022-05-18 10:14:06 +0200

    - small change to output result

--- first-project/index.js
+++ first-project/index.js
@@ -1,9 +1,12 @@
 var button = document.querySelector("button");
 var input1 = document.getElementById("num1");
 var input2 = document.getElementById("num2");
+var output = document.getElementById("output");
 function add(num1, num2) {
     return +num1 + +num2;
 }
 button.addEventListener("click", function () {
-    console.log(add(+input1.value, +input2.value));
+    var result = add(+input1.value, +input2.value);
+    console.log(result);
+    output.innerHTML = result.toLocaleString();
 });
--- first-project/index.ts
+++ first-project/index.ts
@@ -1,11 +1,14 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1")! as HTMLInputElement;
-const input2 = document.getElementById("num2")! as HTMLInputElement;
+var button = document.querySelector("button");
+var input1 = document.getElementById("num1")! as HTMLInputElement;
+var input2 = document.getElementById("num2")! as HTMLInputElement;
+var output = document.getElementById("output")! as HTMLDivElement;
 
 function add(num1: number, num2: number) {
     return +num1 + +num2;
 }
 
 button.addEventListener("click", function() {
-  console.log(add(+input1.value, +input2.value));
+  let result = add(+input1.value, +input2.value)
+  console.log(result);
+  output.innerHTML = result.toLocaleString();
 });
