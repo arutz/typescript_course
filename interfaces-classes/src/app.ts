085bb804282705cd6ccace7af107217fcbecbd2a
arutz <achim.rutz@googlemail.com>
2022-06-10 14:16:54 +0200

    easier startup through paralleel start of tsc and lite-server in npm run script

--- interfaces-classes/src/app.ts
+++ interfaces-classes/src/app.ts
@@ -1,7 +1,40 @@
-// Code goes here!
+// fun with interfaces and classes
 
-function test() {
-    console.log("test");
-}
+class Department {
+    private _name: string = "";
 
-(document.querySelector("#button") as HTMLButtonElement).addEventListener("click", test.bind(this));
+    public get name(): string {
+        return this._name;
+    }
+    public set name(value: string) {
+        this._name = value;
+    }
+
+    private _id: number = -1;
+
+    public get id(): number {
+        return this._id;
+    }
+    public set id(value: number) {
+        this._id = value;
+    }
+
+    // #region builders
+
+    public bName(value: string): Department {
+        this.name = value;
+        return this;
+    }
+
+    public bId(value: number): Department {
+        this.id = value;
+        return this;
+    }
+
+    // #endregion builders
+};
+
+let dep = new Department().bName("Development").bId(1337);
+
+
+console.log(dep);
