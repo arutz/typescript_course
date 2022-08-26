ffe61b5d4b2f753b89781d7e63b8b1eb28601ed4
rutz-4 <achim.rutz@mt.com>
2022-08-26 10:43:11 +0200

    finished chapter 7

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,16 +1,16 @@
 /*  generics for an array which has a union; notice that due to the usage of generics
-    we can tell that it is either a number or string */
+    we can tell that it contains either a number or string (or both) */
 const names: Array<string|number> = ["max", "maxine", 4];
 console.log(names[2]);
 
 /*  usage of generics for parameter types; notice that "merged" knows which attributes exist at compile time (would no be possible without generics).
-    also notice the "extends" which acts as a "constraint" for the generic type */
+    also notice the "extends" which acts as a "constraint" for the generic type (not possible if one "just" uses object) */
 function merge<T extends object, U extends object>(objA: T, objB: U) {
     return Object.assign(objA,objB);
 }
 
 const merged = merge({ name: "Max", hobbies: ['biking', 'swimming']}, { age: 30 });
-console.log(merged);
+console.log(merged.name);
 
 /*  This demonstrates a case where we use a generic type that requires a "length" attribute.
     Notice that the calls at the end do not have to extend or implement the actual interface */
@@ -68,6 +68,30 @@ const anotherStorage = new DataStorage<PersonData>();
 anotherStorage.addItem({ name: "Max"});
 anotherStorage.addItem({ name: "Oldy", age: 30});
 anotherStorage.removeItem({ name: "Max"}); // note that this will not remove the "Max" entry since the collection will work with object references
-// => only assigning "Max" to a variable and then remove that variable will work´for this case
+// => only assigning "Max" to a variable and then remove that variable will work for this case => a distinction for types and specialized deletion
+// algorithm will have to be written
 console.log(anotherStorage.getItems());
+
+/* utility types relating to generics */
+interface CourseGoal {
+    title: string,
+    description: string,
+    completeUntil: Date
+}
+
+// demonstration of "Partial" utility type => used to "eventually" build another type
+function createCourseGoal(title: string, description: string, completeUntil: Date): CourseGoal {
+    let goal: Partial<CourseGoal> = {}; 
+    goal.title = title;
+    goal.description = description;
+    goal.completeUntil = completeUntil;
+    return goal as CourseGoal;
+}
+
+console.log(JSON.stringify(createCourseGoal("typescript","most awesome ts course ever", new Date())));
+
+// demonstration of readonly utility type
+const nameArr: Readonly<string[]> = ["Max", "Anna"];
+// nameArr.push("Manu"); => will not work bc of Readonly utility type
+// nameArr.pop(); => will not work bc of Readonly utility type
 
